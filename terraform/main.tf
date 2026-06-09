4566f53bc0269551e04db199c28b395e86752d53
Immanuel Kituku <immanuelkituku@gmail.com>
2026-06-09 08:28:50 +0300

    Improving on S3 code

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,14 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-bucket"{
-    source = "terraform-aws-modules/s3-bucket/aws"
-    version = "~> 5.14.0"
+module "s3-bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 5.14.0"
 
-    bucket = "test-bucket-1234567890"
+  bucket = var.bucket_name
+  attach_policy = true
+  website = {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
 }
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "eu-west-1"
 }
+
+variable "bucket_name" {
+  description = "The name of the S3 bucket to create"
+  type        = string
+  default     = "test-bucket-2541"
+}
